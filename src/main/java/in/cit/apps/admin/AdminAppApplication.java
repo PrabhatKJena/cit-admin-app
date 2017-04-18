e1b0abefa0850e07f61cc6cc4d61fa3598ec2e02
Prabhat Jena <prajena@paypalcorp.com>
2017-04-18 19:39:09 +0530

    Initial commit

---
+++ src/main/java/in/cit/apps/admin/AdminAppApplication.java
@@ -0,0 +1,12 @@
+package in.cit.apps.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAppApplication.class, args);
+	}
+}
