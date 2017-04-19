d58dd1f6756a255d2260974afe53265335b40cab
Prabhat <jenaprabhat@gmail.com>
2017-04-19 21:40:36 +0530

    updated static script

--- scripts/static_data.sql
+++ scripts/static_data.sql
@@ -1,5 +1,5 @@
 -- static data for USER_GROUPS
 INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('ADMIN', 'Admin group will have all access and role control');
-INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('RECRUIT', 'TA group will have only access to Consultant and Client resources');
-INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('FINANCE', 'TA group will have only access to Consultant and Client resources');
-INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('ALL', 'Admin group will have all access and role control');
+INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('RECRUIT', 'This group will have only access to Consultant and Client resources');
+INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('FINANCE', 'This group will have RECRUIT roles and Financial roles');
+INSERT INTO USER_GROUPS (GROUP_NAME,GROUP_DESC) VALUES ('ALL', 'This is the super user having access to all resources');
